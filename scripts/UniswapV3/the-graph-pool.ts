29feaa16e3761202fb5e56c6b4f68b43545b2e06
zapaz.eth <alain@kredeum.com>
2022-02-07 22:36:08 +0100

    calculate G-UNI pool tokens price

--- scripts/UniswapV3/the-graph-pool.ts
+++ scripts/UniswapV3/the-graph-pool.ts
@@ -4,12 +4,18 @@ import type { Response } from "node-fetch";
 import fetch from "node-fetch";
 import { ethers, BigNumber, FixedNumber } from "ethers";
 
-const uniswapV3API = "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3";
-const gql = `{
-  pools(where: {id: "0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8"}) {
+const api = "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3";
+const gql = `
+{
+  pools(where: {id: "0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640"}) {
     id
-    totalValueLockedUSD
+    token0Price
     liquidity
+    feeTier
+    totalValueLockedUSD
+    untrackedVolumeUSD
+    volumeUSD
+    sqrtPrice
     token0Price
     token1Price
     token0 {
@@ -19,14 +25,15 @@ const gql = `{
       symbol
     }
   }
-}`;
+}
+`;
 
 const runQuery = async (url: string, query: Object): Promise<string> => {
   const res: Response = await fetch(url, { method: "POST", body: JSON.stringify({ query }) });
   return await res.json();
 };
 
-runQuery(uniswapV3API, gql)
+runQuery(api, gql)
   .then((res: any) => {
     console.log(JSON.stringify(res, null, "  "));
 
