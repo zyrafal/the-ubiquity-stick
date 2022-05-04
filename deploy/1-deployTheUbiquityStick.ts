27bde7ddbaeba2bda82cb39ca3ea30b9e5d85177
zapaz.eth <alain@kredeum.com>
2022-05-04 13:20:39 +0200

    cheaper deploy, with args in constructor

--- deploy/1-deployTheUbiquityStick.ts
+++ deploy/1-deployTheUbiquityStick.ts
@@ -1,5 +1,4 @@
 import type { DeployFunction } from "hardhat-deploy/types";
-import type { TheUbiquityStick } from "types/TheUbiquityStick";
 import tokenURIs from "../metadata/json.json";
 
 const deployTheUbiquityStick: DeployFunction = async function ({ ethers, deployments, getNamedAccounts }) {
@@ -7,20 +6,11 @@ const deployTheUbiquityStick: DeployFunction = async function ({ ethers, deploym
   const deployer = await ethers.getNamedSigner("deployer");
   console.log("deployer", deployer.address);
 
-  const ten = ethers.BigNumber.from("10");
-  const gwei = ten.pow(9);
-
-  const deployResult = await deploy("TheUbiquityStick", {
+  await deploy("TheUbiquityStick", {
     from: deployer.address,
-    args: [],
+    args: [[tokenURIs.standardJson, tokenURIs.goldJson, tokenURIs.invisibleJson]],
     log: true
   });
-  if (deployResult.newlyDeployed) {
-    const theUbiquityStick = new ethers.Contract(deployResult.address, deployResult.abi, deployer) as TheUbiquityStick;
-    await theUbiquityStick.setTokenURI(0, tokenURIs.standardJson);
-    await theUbiquityStick.setTokenURI(1, tokenURIs.goldJson);
-    await theUbiquityStick.setTokenURI(2, tokenURIs.invisibleJson);
-  }
 };
 export default deployTheUbiquityStick;
 
